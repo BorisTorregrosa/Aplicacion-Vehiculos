8dcffd4a90c85d2cd65333b672c2daafdabcecb8
BorisTorregrosa <torregrosaborisjose@gmail.com>
2026-05-24 02:26:46 -0500

    Cambio en el port

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({ port: 3000 });
+const PORT = process.env.PORT || 3000;
+const server = new WebSocket.Server({ port: PORT });
 
-console.log("Servidor encendido");
+console.log("Servidor encendido en puerto", PORT);
 
 server.on('connection', (socket) => {
-
     console.log("Cliente conectado");
 
     socket.on('message', (msg) => {
-
         server.clients.forEach((client) => {
-
             if(client.readyState === WebSocket.OPEN){
-
                 client.send(msg.toString());
+            }
+        });
+    });
+});
 
             }
 
